ef630dd2510352ca33f10c71f5c84abae6f8ec13
Kevin McNeil <dogwoodrawks@gmail.com>
2020-08-04 12:58:38 -0700

    added info about me in html

---
+++ javascript/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var userName = prompt('What is your name?');
+console.log('the user\'s name is' + userName);
+alert('Welcome ' + userName + '!' + ' My name is Kevin!')
+document.write('<h2>' + 'Hellow' + userName + '</h2>');
+Document.write('Welcome to my website all about me!')
