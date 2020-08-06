ce71d69b89c7bde1da6db888c145ce70d0bdb5a8
Kevin McNeil <dogwoodrawks@gmail.com>
2020-08-05 18:04:24 -0700

    added some changes to css

--- javascript/app.js
+++ javascript/app.js
@@ -4,5 +4,5 @@ var userName = prompt('What is your name?');
 // console.log('the user\'s name is' + userName);
 alert('Welcome ' + userName + '!' + ' My name is Kevin!')
 alert('there will be a quiz at the end so be sure to read this page carefully!');
-document.write('<h2>' + 'Hellow ' + userName + '</h2>');
+document.write('<h2>' + 'Hello ' + userName + '</h2>');
 document.write('Welcome to my website all about me!')
